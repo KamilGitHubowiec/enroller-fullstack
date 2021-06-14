01f1357c63c23068369b39d60ca798ee76de7b10
KamilGitHubowiec <hantsky123@gmail.com>
2021-06-14 16:10:20 +0200

    changes

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from "vue-resource";
 
 new Vue({
     el: '#app',
     render: h => h(App)
 });
+Vue.use(VueResource);
+Vue.http.options.root = '/api';
